955a3095fee49741eec63994d3e8446808477c30
annaannmathew <annaam9302@gmail.com>
2025-11-24 12:50:18 +0530

    role crud completed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,6 +15,28 @@ use App\Http\Controllers\Api\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserRoleController;
+use App\Http\Controllers\Api\UserController;
+
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'index']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+Route::get('/user-role', [UserRoleController::class, 'index']);
+Route::get('/user-role/{id}', [UserRoleController::class, 'index']);
+Route::post('/user-role', [UserRoleController::class, 'store']);
+Route::put('/user-role/{id}', [UserRoleController::class, 'update']);
+Route::delete('/user-role/{id}', [UserRoleController::class, 'destroy']);
+
+Route::get('/roles', [RoleController::class, 'index']);
+Route::post('/roles', [RoleController::class, 'store']);
+Route::put('/roles/{id}', [RoleController::class, 'update']);
+Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
+
+
 
 Route::post('/login', [AuthController::class, 'login']);
 
